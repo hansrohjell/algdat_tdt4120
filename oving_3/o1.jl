fc2e0509f1ae6303a99239a61a4dd1511af0a684
Hansrohjello <hansrohjello@gmail.com>
2019-09-18 15:36:37 +0200

    oppdatert

---
+++ oving_3/o1.jl
@@ -0,0 +1,9 @@
+#x og y er to sorterte arrays, coordinate angir koordinat akse
+function mergearrays(x,y,coordinate)
+
+end
+
+#x usortert array, coordinate angir koordinat akse vi skal sortere langs
+function mergesort(x, coordinate)
+
+end
